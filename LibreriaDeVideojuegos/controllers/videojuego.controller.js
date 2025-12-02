772eea8d776aad219495b8e7facb0b3dd8fd682d
Dav1dManzanares <davidmanzanares693@gmail.com>
2025-12-02 11:40:02 -0600

    requisito 5, 6 y 7

--- LibreriaDeVideojuegos/controllers/videojuego.controller.js
+++ LibreriaDeVideojuegos/controllers/videojuego.controller.js
@@ -2,11 +2,25 @@ const Videojuego = require("../models/Videojuego")
 
 exports.obtenerVideojuego = async (req, res) => {
     try{
-        const Videojuegos = await Videojuego.find();
+        const {excluirdesarrollador, proyectar, lanzamiento} = req.query;
+        const filtro = {};
+        if (excluirdesarrollador) {
+           filtro.desarrollador = { $ne: excluirdesarrollador };
+        }
+
+        let query = Videojuego.find(filtro);
+
+        if (proyectar) {
+            query.select("titulo fecha_lanzamiento desarrollador");
+        }
+        if (lanzamiento) {
+            query.sort({ fecha_lanzamiento: 1 });
+        }
+
+        const Videojuegos = await query;
+
         res.json(Videojuegos);
-        console.log(Videojuegos)
     }catch (error) {
         res.status(500).json({error: error.message});
     }
 };
-
